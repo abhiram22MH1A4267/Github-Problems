fdda650e12cb4552a1652e5d8cc1fabc8ee5a1ed
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-03-18 08:19:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    bool equalPartition(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int sum = accumulate(arr.begin(), arr.end(), 0);
+        if((sum & 1) != 0) return false;
+        
+        int target = sum /2;
+        
+        vector<bool>dp(target + 1, false);
+        
+        dp[0] = true;
+        for(int i : arr){
+            for(int j = target; j >= i; j--){
+                dp[j] = dp[j] || dp[j - i];
+            }
+        }
+        
+        return dp[target];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        if (ob.equalPartition(arr))
+            cout << "true\n";
+        else
+            cout << "false\n";
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
