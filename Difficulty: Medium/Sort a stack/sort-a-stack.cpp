0dce2f4f6ee2d53dd443e72775cb591343e4b6a2
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-03-18 10:24:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+
+void printStack(stack<int> s)
+{
+    while (!s.empty())
+    {
+        printf("%d ", s.top());
+       	s.pop();
+    }
+    printf("\n");
+}
+
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+	SortedStack *ss = new SortedStack();
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+	int k;
+	cin>>k;
+	ss->s.push(k);
+	}
+	ss->sort();
+	printStack(ss->s);
+
+cout << "~" << "\n";
+}
+}
+// } Driver Code Ends
+
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack :: sort()
+{
+   //Your code here
+   
+   stack<int>st;
+   while(!s.empty()){
+       vector<int>arr;
+       while(!st.empty() && s.top() < st.top()){
+           arr.push_back(st.top());
+           st.pop();
+       }
+       st.push(s.top());
+       s.pop();
+       for(int i = arr.size()-1; i >= 0; i--){
+           st.push(arr[i]);
+       }
+   }
+   
+   while(!st.empty()){
+       cout<<st.top()<<" ";
+       st.pop();
+   }
+}
