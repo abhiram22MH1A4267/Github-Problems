d43268f9f6fc91a47b14f39db3e4efa68c6d3cfe
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-04-01 09:19:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -4,9 +4,10 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    void dfs(int node, vector<vector<int>>&adj, vector<int>&vis, vector<int>&ans){
+  void dfs(int node, vector<vector<int>>&adj, vector<int>&vis, vector<int>&ans){
         vis[node] = 1;
         ans.push_back(node);
         for(int i : adj[node]){
@@ -15,8 +16,7 @@ class Solution {
             }
         }
     }
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(vector<vector<int>>& adj) {
+    vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
         int n = adj.size();
         vector<int>vis(n,0);
@@ -26,27 +26,34 @@ class Solution {
     }
 };
 
+
 //{ Driver Code Starts.
 
 int main() {
     int tc;
     cin >> tc;
+    cin.ignore();
     while (tc--) {
-        int V, E;
-        cin >> V >> E;
-
+        int V;
+        cin >> V;
+        cin.ignore();
         vector<vector<int>> adj(
             V); // Use vector of vectors instead of array of vectors.
 
-        for (int i = 0; i < E; i++) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        for (int i = 0; i < V; i++) {
+            string input;
+            getline(cin, input);
+            int num;
+            vector<int> node;
+            stringstream ss(input);
+            while (ss >> num) {
+                node.push_back(num);
+            }
+            adj[i] = node;
         }
 
         Solution obj;
-        vector<int> ans = obj.dfsOfGraph(adj);
+        vector<int> ans = obj.dfs(adj);
         for (int i = 0; i < ans.size(); i++) {
             cout << ans[i] << " ";
         }
