20791ad536ee8eeddfff378ce8ec2b5960b2ae4f
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-04-21 07:30:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.cpp
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -1,43 +1,45 @@
 //{ Driver Code Starts
-// Initial template for C++
-
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
-// User function template for C++
+
 class Solution {
   public:
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, vector<int>& arr) {
-
-        // Your code goes here
-        int sum = 0;
-        for(int i = 0; i < n-1; i++){
-            sum += arr[i];
-        }
-        int total = (n*(n+1))/2;
+    int missingNum(vector<int>& arr) {
+        // code here
+        long long int n = arr.size() + 1, sum = accumulate(arr.begin(), arr.end(), 0);
+        long long int total = (n*(n+1))/2;
         return total-sum;
     }
 };
+
 
 //{ Driver Code Starts.
 
 int main() {
     int t;
     cin >> t;
+    cin.ignore(); // to ignore the newline after the integer input
     while (t--) {
         int n;
-        cin >> n;
+        vector<int> a;
+        string input;
 
-        vector<int> arr(n - 1);
-        for (int i = 0; i < n - 1; ++i)
-            cin >> arr[i];
+        // Input format: first number n followed by the array elements
+        getline(cin, input);
+        stringstream ss(input);
+        int num;
+        while (ss >> num)
+            a.push_back(num);
+
         Solution obj;
-        cout << obj.missingNumber(n, arr) << "\n";
+        cout << obj.missingNum(a) << endl;
+        cout << "~\n";
     }
+
     return 0;
 }
+
 // } Driver Code Ends
