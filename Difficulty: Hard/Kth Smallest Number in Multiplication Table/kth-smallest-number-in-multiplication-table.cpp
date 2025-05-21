cae402e992060337391239bd0c71e33e720ebef7
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-05-21 09:00:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Kth Smallest Number in Multiplication Table/kth-smallest-number-in-multiplication-table.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int kthSmallest(int m, int n, int k) {
+        // code here
+        int low = 0, high = m*n;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            int count = 0;
+            for(int i = 1; i <= m; i++){
+                if(n < (mid/i)){
+                    count += n;
+                }
+                else{
+                    count += (mid/i);
+                }
+            }
+            if(count >= k){
+                high = mid;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+};
