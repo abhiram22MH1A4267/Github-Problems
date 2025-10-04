9df9a6a83a5fe3b5320ca436c02860b0cfd5b4ca
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-10-04 10:51:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Expression Add Operators/expression-add-operators.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    void buildExpr(vector<string>& res, string expr, string digits, int target, int idx, int eval, int last) {
+
+        if (idx == digits.length()) {
+            if (eval == target)
+                res.push_back(expr);
+            return;
+        }
+    
+        for (int i = idx; i < digits.length(); i++) {
+
+            if (i != idx && digits[idx] == '0') break;
+    
+            string part = digits.substr(idx, i - idx + 1);
+            int num = stoll(part);
+    
+            if (idx == 0) {
+                buildExpr(res, part, digits, target, i + 1, num, num);
+            } else {
+                buildExpr(res, expr + "+" + part, digits, target, i + 1, eval + num, num);
+                buildExpr(res, expr + "-" + part, digits, target, i + 1, eval - num, -num);
+                buildExpr(res, expr + "*" + part, digits, target, i + 1,
+                          eval - last + last * num, last * num);
+            }
+        }
+    }
+
+    vector<string> findExpr(string digits, int target) {
+        vector<string> res;
+        buildExpr(res, "", digits, target, 0, 0, 0);
+        return res;
+    }
+};
