6ce25ac1721a0495eb20c7e1c137b3032f9de66d
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-10-12 16:10:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.cpp
@@ -0,0 +1,61 @@
+/*
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int x) {
+        data = x;
+        left = right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int distCandy(Node* root) {
+        // code here
+        if (root == nullptr) return 0;
+        int ans = 0;
+    
+        stack<pair<Node*, int>> stk;
+    
+        unordered_map<Node*, int> balance;
+    
+        stk.push({root, 0});
+    
+        while (!stk.empty()) {
+    
+            auto curr = stk.top();
+            auto node = curr.first;
+            auto state = curr.second;
+            stk.pop();
+    
+            if (node == nullptr) continue;
+    
+            if (state == 0) {
+    
+                stk.push({node, 1});
+    
+                stk.push({node->left, 0});
+    
+                stk.push({node->right, 0});
+    
+            } 
+            else {
+    
+                int leftBalance = balance[node->left];
+    
+                int rightBalance = balance[node->right];
+    
+                ans += abs(leftBalance) + abs(rightBalance);
+    
+                balance[node] 
+                  = node->data + leftBalance + rightBalance - 1;
+            }
+        }
+      
+        return ans;
+    }
+};
