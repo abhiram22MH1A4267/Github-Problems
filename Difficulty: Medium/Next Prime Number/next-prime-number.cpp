9fbf2f42da5ba9cdd6e40191c9de6b2848001746
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-11-04 15:21:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Prime Number/next-prime-number.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+void getprimes(vector<int>&primes){
+    primes[0] = primes[1] = 0;
+    
+    for(int i = 2; i <= (501 / 10); i++){
+        for(int j = i+i; j < 501; j += i){
+            primes[j] = 0;
+        }
+    }
+}
+int nextPrime(int n) {
+
+    // code here to find next prime number
+    // return next prime number
+    
+    vector<int>primes(501, 1);
+    getprimes(primes);
+    
+    for(int i = n+1; i < 501; i++){
+        if(primes[i] == 1) return i;
+    }
+    
+    return 0;
+}
