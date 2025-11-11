5784b35fd303c021d26782e9b8efc5ad672ef676
Abhiram <155733378+abhiram22MH1A4267@users.noreply.github.com>
2025-11-11 08:46:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int minSuperSeq(string &s1, string &s2) {
+        // code here
+        int m = s1.size(), n = s2.size();
+
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+    
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                
+                if (s1[i - 1] == s2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+    
+        int lcsLen = prev[n];
+    
+        return m + n - lcsLen;
+    }
+};
